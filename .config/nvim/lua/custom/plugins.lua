8ccb532bdc35827254b9c8e487736c8f878876a3
Pavel Slepushkin <pavel.slepushkin@gmail.com>
2023-01-01 19:09:36 +0100

    add nvim configration

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,24 @@
+return function(use)
+  use({
+    "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup({})
+      end
+  },
+  use{'ray-x/go.nvim',
+      config = function ()
+        require('go').setup()
+      end
+    },
+--  use 'shaunsingh/nord.nvim',
+--  use 'arcticicestudio/nord-vim',
+--  use 'Roy-Orbison/nord-vim-256',
+  use{
+   'windwp/nvim-autopairs',
+     config =function ()
+       require('nvim-autopairs').setup()
+     end
+    },
+    use{'ThePrimeagen/vim-be-good'}
+  )
+end
