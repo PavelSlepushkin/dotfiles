18027ffb09c4866aa28f2d1bc66658976b3c0b29
Pavel Slepushkin <pavel.slepushkin@gmail.com>
2024-01-14 15:38:46 +0100

    move bat theme from .zshenv

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias k='kubectl'
 # short alias to set/show context/namespace (only works for bash and bash-compatible shells, current context to be set before using kn to set namespace)
 # alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
 # alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+# make kn a function, so we can create completion without setopt completealaiases
 function kn() {
     [[ -v 1 ]] && kubectl config set-context --current --namespace "$1" || \
         kubectl config view --minify | grep namespace | cut -d" " -f6 
@@ -23,5 +24,6 @@ alias la="ls -la"
 ## trying some modern replacements
 alias cat=batcat
 alias catp=batcat --plain
+export BAT_THEME=Nord
 alias diff=delta
 compdef delta=diff
