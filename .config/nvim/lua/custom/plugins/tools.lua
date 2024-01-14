ba08b0f122b319aad96306511b8124f07e337ffa
Pavel Slepushkin <pavel.slepushkin@gmail.com>
2024-01-14 16:55:00 +0100

    switch markdown preview to non-yarn build

--- .config/nvim/lua/custom/plugins/tools.lua
+++ .config/nvim/lua/custom/plugins/tools.lua
@@ -54,14 +54,10 @@ return {
     end
   },
   'towolf/vim-helm',
-  -- install with yarn or npm
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
     ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
   },
 }
