29a2fec22b57c92294ef91c5113a507ad6ddcc48
Pavel Slepushkin <pavel.slepushkin@kyriba.com>
2024-04-22 17:41:01 +0200

    switch macos to kickstarter

--- .config/nvim/lua/custom/keymaps.lua
+++ .config/nvim/lua/custom/keymaps.lua
@@ -1,13 +1,13 @@
 vim.opt.relativenumber = true
 --bash-like tabComplete
-vim.opt.wildmode = "longest,list,full"
+vim.opt.wildmode = 'longest,list,full'
 vim.opt.wildmenu = true
 
 vim.opt.scrolloff = 8
 
 -- Ctrl-s for save
-vim.keymap.set("n", "<C-s>", ":w<CR>")
-vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>')
 -- leader-s for search and replace current word
 --vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
@@ -16,27 +16,32 @@ vim.keymap.set('x', '<Leader>/', '<Esc>/\\%V', { desc = 'Search in visial block'
 vim.keymap.set('x', '<Leader>r', [[:s/\%V]], { desc = '[R]eplace in visual block' })
 
 --Primagen remaps - move Visual block with JK
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 -- same but for cursor keys
-vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<S-Up>', ":m '<-2<CR>gv=gv")
 -- cursor case up/down - go into Normal mod
-vim.keymap.set("i", "<Down>", "<Esc><Down>")
-vim.keymap.set("i", "<Up>", "<Esc><Up>")
+vim.keymap.set('i', '<Down>', '<Esc><Down>')
+vim.keymap.set('i', '<Up>', '<Esc><Up>')
 
 -- Join lines with keep cursor place - commented out at the moment
 -- vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set('x', '<leader>p', '"_dP')
+-- disable clipboard integration
+vim.opt.clipboard = ''
 -- yank into system clipboard
-vim.keymap.set({ "n", "v" }, "y", "\"*y")
+--macOS, linux X11
+-- vim.keymap.set({ "n", "v" }, "y", "\"*y")
+-- linux Wayland
+vim.keymap.set({ 'n', 'v' }, 'y', '"+y')
 --[[ open telescope find_files when vim started without paramters
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
@@ -47,41 +52,43 @@ vim.api.nvim_create_autocmd('VimEnter', {
 })
 --]]
 --autocommand for save on focust lost and BufLeave
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost' }, {
   callback = function()
-    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
-      vim.api.nvim_command('silent update')
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand '%' ~= '' and vim.bo.buftype == '' then
+      vim.api.nvim_command 'silent update'
     end
   end,
 })
 
-vim.keymap.set('n', '<leader>t', function() vim.o.rnu = not vim.o.rnu end, { desc = '[T]oggle relative line numbers' })
+vim.keymap.set('n', '<leader>t', function()
+  vim.o.rnu = not vim.o.rnu
+end, { desc = '[T]oggle relative line numbers' })
 
 --
 --Setup for Notes, assumes folder ~/notes/ exists
 --
 local Notes = {}
 -- create functions for keymaps
-Notes.Path = os.getenv("HOME") .. "/notes/"
+Notes.Path = os.getenv 'HOME' .. '/notes/'
 Notes.Log = function()
-  vim.cmd("e " .. Notes.Path .. "log.txt")
+  vim.cmd('e ' .. Notes.Path .. 'log.txt')
 end
 Notes.Scratch = function()
-  vim.cmd("e " .. Notes.Path .. "scratch.txt")
+  vim.cmd('e ' .. Notes.Path .. 'scratch.txt')
 end
 Notes.Daily = function()
-  vim.cmd("e " .. Notes.Path .. os.date("%Y-%m-%d") .. ".md")
+  vim.cmd('e ' .. Notes.Path .. os.date '%Y-%m-%d' .. '.md')
 end
 Notes.Find_Files = function()
   require('telescope.builtin').find_files {
     prompt_title = '<notes::files>',
-    cwd = Notes.Path
+    cwd = Notes.Path,
   }
 end
 Notes.Live_Grep = function()
   require('telescope.builtin').live_grep {
     prompt_title = '<notes::grep>',
-    cwd = Notes.Path
+    cwd = Notes.Path,
   }
 end
 -- settings keymaps
--- .config/nvim/lua/custom/plugins/tools.lua
+++ .config/nvim/lua/custom/plugins/tools.lua
@@ -2,62 +2,55 @@
 return {
   {
     'echasnovski/mini.cursorword',
-    branch = 'stable', --underline word under cursor
+    version = '*', --underline word under cursor
     config = function()
       require('mini.cursorword').setup()
-    end
+    end,
   },
 
-  {
-    "kylechui/nvim-surround", --surround -add keymaps like 'ys(' - you surround, 'cs]' - change surround to ]
-    version = "*",            -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  },
+  -- {
+  --   "kylechui/nvim-surround", --surround -add keymaps like 'ys(' - you surround, 'cs]' - change surround to ]
+  --   version = "*",            -- Use for stability; omit to use `main` branch for the latest features
+  --   config = function()
+  --     require("nvim-surround").setup({
+  --       -- Configuration here, or leave empty to use defaults
+  --     })
+  --   end
+  -- },
 
-  {
-    'windwp/nvim-autopairs', --autopairs (used to them in VS code)
-    config = function()
-      require('nvim-autopairs').setup()
-    end
-  },
-
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-
-  {
-    'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
-    config = function()
-      require('Comment').setup()
-    end
-  },
+  -- {
+  --   'windwp/nvim-autopairs', --autopairs (used to them in VS code)
+  --   config = function()
+  --     require('nvim-autopairs').setup()
+  --   end
+  -- },
 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   -- 'tpope/vim-unimpaired', -- handy mappins for [ and ]
-  {
-    'tummetott/unimpaired.nvim', --same but in lua with which-key support
-    config = function()
-      require('unimpaired').setup {
-        -- add any options here or leave empty
-      }
-    end
-  },
+  -- {
+  --   'tummetott/unimpaired.nvim', --same but in lua with which-key support
+  --   config = function()
+  --     require('unimpaired').setup {
+  --       -- add any options here or leave empty
+  --     }
+  --   end
+  -- },
   {
     'jinh0/eyeliner.nvim',
     config = function()
-      require 'eyeliner'.setup {
+      require('eyeliner').setup {
         highlight_on_key = true, -- show highlights only after keypress
-        dim = true               -- dim all other characters if set to true (recommended!)
+        dim = true, -- dim all other characters if set to true (recommended!)
       }
-    end
+    end,
   },
   'towolf/vim-helm',
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
   },
 }
