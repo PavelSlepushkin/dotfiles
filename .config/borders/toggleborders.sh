c3d73923c64e15fb43f4d6dfd1ba8d87eb221a56
Pavel Slepushkin <pavel.slepushkin@kyriba.com>
2024-06-27 16:37:21 +0200

    switch finicky to chrome with profiles

--- .config/borders/toggleborders.sh
+++ .config/borders/toggleborders.sh
@@ -2,6 +2,8 @@
 windowID=$(yabai -m query --windows --window | jq '.id')
 if $(yabai -m query --windows --window |jq '."has-fullscreen-zoom"'); then 
   borders active_color=0xffff0000 apply-to=${windowID}
+elif $(yabai -m query --windows --window |jq '."has-parent-zoom"'); then 
+  borders active_color=0xffab47bc apply-to=${windowID}
 else
   borders active_color=0xffff7600 apply-to=${windowID}
 fi
---
+++ .config/finicky/finicky.js
@@ -0,0 +1,57 @@
+// ~/.finicky.js
+const chromeOkta = {
+  name: "Google Chrome",
+  profile: "System Profile",
+};
+
+const chromeCloud = {
+  name: "Google Chrome",
+  profile: "Profile 1",
+};
+
+
+module.exports = {
+  //legacy :)
+  //need to switch to chrome back
+  defaultBrowser: "Firefox",
+  // rewrite: [
+  //   {
+  //     // Redirect all urls to use https
+  //     match: ({ url }) => url.protocol === "http",
+  //     url: { protocol: "https" }
+  //   }
+  // ],
+  handlers: [
+    {
+      // Open apple.com and example.com urls in Safari
+      match: finicky.matchHostnames(["apple.com", "example.com"]),
+      browser: "Safari"
+    },
+    {
+      // Open TFE in Brave - cloud accounts
+      // argo also should go via cloud account at the moment
+      // https://tfe.devops.kyriba.com
+      match: finicky.matchHostnames(["tfe.devops.kyriba.com", "argocd.core.aws.kyriba.com"]),
+      browser: chromeCloud,
+    },
+    {
+      // Open any url that includes the string "workplace" in Firefox
+      match: /workplace/,
+      browser: "Firefox"
+    },
+    {
+      // Argo prod now works in Chrome - but not with admin creds
+      match: finicky.matchHostnames(["ro-argocd.core.aws.kyriba.com", "gitlab.com", "argocd.dev.kod.kyriba.com","localhost"]),
+      browser: chromeOkta,
+    },
+    {
+      // Open google.com and *.google.com urls in Google Chrome
+      // This was in use mostly for meet, keeping it up as legacy
+      match: [
+        "google.com/*", // match google.com urls
+        "*.google.com/*", // match google.com subdomains
+      ],
+      browser: chromeOkta,
+    }
+  ]
+};
